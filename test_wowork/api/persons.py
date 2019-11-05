caa46a3c53771d970f93d2eb507d0d7c23f29209
smellwithsun <1354350240@qq.com>
2019-11-05 16:26:34 +0800

    增加部门成员模块

---
+++ test_wowork/api/persons.py
@@ -0,0 +1,24 @@
+
+class Persons:
+    def created_person(self):
+        pass
+    def read_person(self):
+        pass
+    def update_person(self):
+        pass
+    def delete_person(self):
+        pass
+    def all_delete_person(self):
+        pass
+    def get_department_persons(self):
+        pass
+    def get_department_person_detail(self):
+        pass
+    def exchange(self):
+        pass
+    def two_verify(self):
+        pass
+    def invite_person(self):
+        pass
+    def get_decode(self):
+        pass
---
+++ test_wowork/testcase/test_persons.py
@@ -0,0 +1,5 @@
+from test_wowork.api.persons import Persons
+
+
+class TestPersons:
+    person = Persons()
